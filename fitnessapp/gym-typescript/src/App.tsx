dd7a09b342d587b42836b1d4c15007ed73c1c3c4
noorx7 <nuruli@mun.ca>
2023-01-24 08:54:18 -0330

    Initial Setup of Gym App

---
+++ fitnessapp/gym-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Navbar from "@/scenes/navbar"
+
+function App() {
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar/>
+    </div>
+  )
+}
+
+
+export default App

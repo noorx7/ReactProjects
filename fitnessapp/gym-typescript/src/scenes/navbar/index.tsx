b96fe57948d7b88b441dc365cb1392d536dd4f31
noorx7 <nuruli@mun.ca>
2023-01-25 19:26:32 -0330

    ResponsiveNav1

--- fitnessapp/gym-typescript/src/scenes/navbar/index.tsx
+++ fitnessapp/gym-typescript/src/scenes/navbar/index.tsx
@@ -3,6 +3,8 @@ import {Bars3Icon , XMarkIcon} from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
 import Link from "./Link"
 import { SelectedPage } from "@/shared/types"
+import useMediaQuery from "@/hooks/useMediaQuery"
+import ActionButton from "@/shared/action"
 
 
 
@@ -14,6 +16,8 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
 
   return (  <nav>
         <div
@@ -26,6 +30,7 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                     
 
                     {/* Right Side */}
+                    { isAboveMediumScreens ? (   
                     <div className={`${flexBetween} w-full`}>
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link 
@@ -39,12 +44,49 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                         </div>
                         <div className={`${flexBetween} gap-8`}>
                             <p>Sign In</p>
-                            <button>Become a member</button>
+                            <ActionButton 
+                            setSelectedPage={setSelectedPage}
+                            >
+                                Become a member
+                            </ActionButton>
                         </div>
                     </div>
+                    ):(
+
+                        <button className="rounded-full bg-secondary-500 p-2" onClick={()=>{
+                            setIsMenuToggled(!isMenuToggled)
+                        }}>
+                            <Bars3Icon className="h-6 w-6 text-white"/>
+                        </button>
+                    )}
                 </div>
             </div>
         </div> 
+        {/* Mobile Menu Modelling */}
+        {!isAboveMediumScreens && isMenuToggled && (
+
+                <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                    {/* CLOSE ICON */}
+                    <div className="flex justify-end p-12">
+                        <button onClick={()=> setIsMenuToggled(!isMenuToggled)}>
+                            <XMarkIcon className="h-6 w-6 text-gray-400"/>
+                        </button>
+                    </div>
+                        {/* MENU ITEMS */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                                <Link 
+                                    page="Home" 
+                                    selectedPage={selectedPage} 
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                                <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                                <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                    </div>
+
+                </div>
+            
+        )}
     </nav>
   )
 }

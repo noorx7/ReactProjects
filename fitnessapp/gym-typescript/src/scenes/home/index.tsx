db0bc9e09ec0bdec40ad1328c36dd8e262dbce61
noorx7 <nuruli@mun.ca>
2023-01-28 08:42:56 -0330

    HomeComplete

--- fitnessapp/gym-typescript/src/scenes/home/index.tsx
+++ fitnessapp/gym-typescript/src/scenes/home/index.tsx
@@ -6,7 +6,9 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png"
 import SponsorRedBull from "@/assets/SponsorRedBull.png"
 import SponsorForbes from "@/assets/SponsorForbes.png"
 import SponsorFortune  from "@/assets/SponsorFortune.png"
+
 import AnchorLink from "react-anchor-link-smooth-scroll"
+import { motion } from "framer-motion"
 
 
 type Props = {
@@ -21,22 +23,46 @@ function Home({ setSelectedPage  }: Props) {
     className="gap-16 bg-gray-20 py-10 md:h-full md: pb-0"
     >
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <motion.div 
+        onViewportEnter={()=>{
+          setSelectedPage(SelectedPage.Home)
+        }}
+        
+        className="md:flex mx-auto w-5/6 items-center justify-center  md:h-5/6">
             {/* MAIN HEADER */}
-            <div>
+            <div className="z-10 mt-32 md:basis-3/5">
                   {/* HEADINGS */}
-                  <div>
-                    <div>
-                        <div>
+                  <motion.div 
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{once: true, amount: 0.5}}
+                  transition={{duration: 0.5}}
+                  variants={{
+                    hidden:{opacity:0 , x:-50},
+                    visible: {opacity: 1, x:0}
+                  }}
+                  className="md:-mt-20">
+                    <div className="relative">
+                        <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                           <img alt="home-page-text"  src={HomePageText} />
                         </div>
                       </div>
-                      <p>
+                      <p className="mt-8 text-sm">
                         Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the body Shapes that you Dream of.. Get your Dream Body now
                       </p>
-                  </div>
+                  </motion.div>
                   {/* ACTIONS */}
-                  <div>
+                  <motion.div 
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{once: true, amount: 0.5}}
+                  transition={{delay: 0.4 , duration: 0.5}}
+                  variants={{
+                    hidden:{opacity:0 , x:-50},
+                    visible: {opacity: 1, x:0}
+                  }}
+                  
+                  className="mt-8 flex items-center gap-8 md:justify-start">
                     <ActionButton setSelectedPage={setSelectedPage}>
                       Join Now
                     </ActionButton>
@@ -49,26 +75,25 @@ function Home({ setSelectedPage  }: Props) {
                       <p>Learn More</p>
 
                     </AnchorLink>
-                  </div>
+                  </motion.div>
             </div>
 
             {/* IMAGE HEADER */}
 
-            <div>
+            <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                 <img src={HomePageGraphic} alt="home-pageGraphic" />
             </div>
-        </div>
-        {/* Sponsors */}
-        {isAboveMediumScreens &&(
-          <div>
-            <div>
-              <div>
-                <img src={SponsorRedBull} alt="redbull-sponsor" />
-                <img src={SponsorForbes} alt="forbes-sponsor" />
-                <img src={SponsorFortune} alt="fortune-sponsor" />
-              </div>
+        </motion.div>
+        {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10 ">
+          <div className="flex mx-auto w-5/6 justify-center ">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
             </div>
           </div>
+        </div>
         )}
     </section>
   )
--- fitnessapp/gym-typescript/src/shared/action.tsx
+++ fitnessapp/gym-typescript/src/shared/action.tsx
@@ -7,7 +7,7 @@ type Props = {children : React.ReactNode;  setSelectedPage: (value: SelectedPage
 function ActionButton({ children , setSelectedPage }: Props) {
   return (
     <AnchorLink 
-    className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white" 
+    className=" font-semibold rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white" 
     onClick={()=> setSelectedPage(SelectedPage.ContactUs)}
     href={`#${SelectedPage.ContactUs}`}
     

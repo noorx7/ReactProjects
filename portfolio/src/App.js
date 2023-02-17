9345ed48a8deda934530432d983f845eb5c0062c
noorx7 <nuruli@mun.ca>
2023-02-17 02:15:58 -0330

    50DoneLanding

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,33 @@
-import { useEffect, useState } from "react";
-import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
+import Landing from "./scenes/Landing";
 import DotGroup from "./scenes/DotGroup";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
-  const [isTopOfPage, setIsTopOfPage] = useState(true)
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
       if (window.scrollY !== 0) setIsTopOfPage(false);
-    }
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [])
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-deep-blue">
       <Navbar
         isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage} setSelectedPage={setSelectedPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
       <div className="w-5/6 mx-auto md:h-full">
         {isAboveMediumScreens && (
@@ -28,9 +36,16 @@ function App() {
             setSelectedPage={setSelectedPage}
           />
         )}
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
